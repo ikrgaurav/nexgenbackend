c1065a5c921fe651fddba3b9a17e9de9eec5ffde
ikrgaurav <kkr2810@gmail.com>
2024-11-12 18:21:51 +0530

    port issue

--- app.js
+++ app.js
@@ -28,5 +28,5 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((error) => console.log('MongoDB connection error:', error));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
